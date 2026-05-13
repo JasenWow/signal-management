9f1cb29abd6b1ba9fc44276ad7ad43cc912d3a80
jasenwood <janswu@icloud.com>
2026-05-13 21:26:58 +0800

    feat(routing): add react-router with BrowserRouter setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router'
 import App from './App'
 import '@shared/constants'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/:messageId" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
